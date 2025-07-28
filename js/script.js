752c42d5b2ad50c6aba68fda00c7cad74d692372
dheerajjj <dheeraj.koduru@gmail.com>
2025-07-28 21:33:54 +0530

    advocate add

--- js/script.js
+++ js/script.js
@@ -133,7 +133,7 @@ contactForm.addEventListener('submit', function(e) {
     
     // Create email content
     const emailContent = {
-        to_email: 'aparajitha.law@gmail.com',
+        to_email: 'dheeraj8453@gmail.com',
         from_name: `${firstName} ${lastName}`,
         from_email: email,
         phone: phone,
@@ -157,7 +157,7 @@ contactForm.addEventListener('submit', function(e) {
         })
         .catch(function(error) {
             console.error('EmailJS error:', error);
-            alert('Sorry, there was an error sending your message. Please try again or call us directly at +91-7675058983.');
+            alert('Sorry, there was an error sending your message. Please try again or call us directly at +91-8341445701.');
         })
         .finally(function() {
             submitButton.textContent = originalText;
